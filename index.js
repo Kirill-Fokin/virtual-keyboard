e0319a0ad8dcc165f39a513d833e6a4339e8c16f
Kirill-Fokin <kirill19011998@mail.ru>
2023-05-04 02:35:50 +0300

    feat: correct mouse clock on buttons animation

--- index.js
+++ index.js
@@ -1,28 +1,49 @@
-alert('Добрый день, если будет возможность, отложите, пожалуйста проверку на последний день, хотел доделать некоторые баги, если же нет, то большая чать доступна сейчас деплой от 2 мая, удачи ! по любым вопросам пишите в чат RS либо Discord ');
+ alert('Minimal scope: выполнено \n Basic scope: cмена языка выполненj, но не работает запоминание при рефреше стр, клики мышкой обрабатываются\n extra scope: анимации присутствуют; \n  Technical requirements: ES6 используется: стрелочные ф-ии обрытные ковычки, линтер включен, требования к репозиторию и коммитам выполнены  есть некоторые баги, которые постараюсь пофиксить, и доделать запоминание при рефреше');
+
 let layout = ["`","1","2","3","4","5","6","7","8","9","0","-", "=", "Backspace",
-            "Tab","Q","W","E","R","T","Y","U","I","O","P","[","]","\\","DEL",
-            "Caps Lock","A","S","D","F","G","H","J","K","L",";","'","ENTER",
-            "Shift","~","Z","X","C","V","B","N","M",".",",","/","↑","Shift",
-			      "Ctrl","Win","ALT"," ","ALT","←","↓","→"];
+            "Tab","q","w","e","r","t","y","u","i","o","p","[","]","\\","DEL",
+            "CapsLock","a","s","d","f","g","h","j","k","l",";","'","Enter",
+            "Shift","~","z","x","c","v","b","n","m",".",",","/","↑","Shift",
+			      "Ctrl","Win","Alt"," ","Alt","←","↓","→"];
 
 let Rus_layout = ["ё","1","2","3","4","5","6","7","8","9","0","-","=","Backspace",
-"Tab","й","ц","у","к","е","н","г","ш","щ","з","х","ъ","\\","DEL", "Caps Lock",
-"Caps Lock","ф","ы","в","а","п","р","о","л","д","ж","э","ENTER",
+"Tab","й","ц","у","к","е","н","г","ш","щ","з","х","ъ","\\","DEL", "CapsLock",
+"CapsLock","ф","ы","в","а","п","р","о","л","д","ж","э","Enter",
 "Shift","я","ч","с","м","и","т","ь","б","ю",".","↑","Shift",
-"Ctrl","Win","ALT"," ","ALT","←","↓","→","Ctrl"
+"Ctrl","Win","Alt"," ","Alt","←","↓","→","Ctrl"
 
 ];
 
+let layoutCaps = ["`","1","2","3","4","5","6","7","8","9","0","-", "=", "Backspace",
+            "Tab","Q","W","E","R","T","Y","U","I","O","P","[","]","\\","DEL",
+            "CapsLock","A","S","D","F","G","H","J","K","L",";","'","Enter",
+            "Shift","~","Z","X","C","V","B","N","M",".",",","/","↑","Shift",
+			      "Ctrl","Win","Alt"," ","Alt","←","↓","→"];
+
+let Rus_layotCaps = ["ё","1","2","3","4","5","6","7","8","9","0","-","=","Backspace",
+"Tab","Й","Ц","У","К","Е","Н","Г","Ш","Щ","З","Ч","Ъ","\\","DEL", "CapsLock",
+"CapsLock","Ф","Ы","В","А","П","Р","О","Л","Д","Ж","Э","Enter",
+"Shift","Я","Ч","С","М","И","Т","Ь","Б","Ю",".","↑","Shift",
+"Ctrl","Win","Alt"," ","Alt","←","↓","→","Ctrl"
+
+];
+
+localStorage.setItem('language', 'ENG');
 
 
 
-      
-let bigKeys = ["Backspace", "Caps Lock", "Enter", "Cntrl", "Tab", "Shift",]
+// let layoutLanguage = localStorage.getItem('language')
+let curArray = layout;
+let bigKeys = ["Backspace", "CapsLock", "Enter", "Cntrl", "Tab", "Shift",]
 
-let anotherColorKeys = ["`","DEL","Backspace", "Win","ALT", "↑", "↓", "←", "→", "Caps Lock", "ENTER", "Ctrl", "Tab", "Shift", " "]
-
+let anotherColorKeys = ["`","DEL","Backspace", "Win","Alt", "↑", "↓", "←", "→", "CapsLock", "Enter", "Ctrl", "Tab", "Shift", " "]
 
 const body = document.querySelector('body')
+let keys  = document.querySelectorAll('.key')
+// язык layout
+
+let capsEnaible = false;
+
 
 let mainTitle = document.createElement('h1');
 mainTitle.textContent = 'RSS Виртуальная клавиатура'
@@ -39,8 +60,7 @@ languageIcon.classList.add('language')
 languageIcon.src = "assets/bristish.png"
 body.appendChild(languageIcon)
 
-// язык layout
-let layoutLanguage = 'ENG'
+
 
 //create textArea 
 let textArea = document.createElement('textarea');
@@ -65,7 +85,7 @@ createLine ()
 //advice-block
 let adviceBlock =   document.createElement('div');
 adviceBlock.classList.add('advice')
-adviceBlock.textContent = 'press CNTRL + ALT to change Language клавиутра сделана на MacOS'
+adviceBlock.textContent = 'press cntrl + ALT to change Language клавиутра сделана на MacOS'
 fieldContainer.appendChild(adviceBlock);
 let adviceBlock2 =   document.createElement('div');
 adviceBlock2.classList.add('advice')
@@ -75,49 +95,54 @@ let keyboardLines  = document.querySelectorAll(".keyboard-line")
 //key creation
 
 
-  function initKeys (start , end , line) {
-    let curArray = (layoutLanguage == 'ENG') ? layout : Rus_layout;
-    for (let i = start; i < end; i++) {
-      let newKey = document.createElement('div');
-      newKey.classList.add('key')
-       if ( bigKeys.includes(curArray[i]) ) {
-        newKey.classList.add('key-big') 
-       }
-       if (layout[i] == ' ' ) {
-        newKey.classList.add('key-big2') 
-       }
-       if ( anotherColorKeys.includes(curArray[i]) ) {
-        newKey.classList.add('key-another-color') 
-       }
-       newKey.textContent =`${curArray[i]}`;
-       keyboardLines[line].appendChild(newKey)
+function initKeys (start , end , line) {
+  // let curArray = (layoutLanguage == 'ENG') ? layout : Rus_layout;
+
+  // capsEnaible === false ? curArray: curArray  += `Caps`
+  if (  localStorage.getItem('language') == "ENG") {
+    if (capsEnaible === false) {
+        curArray = layout
+    } else {
+      curArray = layoutCaps
+    }
+  } else {
+    if (capsEnaible === false) {
+       curArray = Rus_layout;
+    } else {
+       curArray = Rus_layotCaps
     }
   }
-   
+
+  for (let i = start; i < end; i++) {
+      let newKey = document.createElement('div');
+      newKey.classList.add('key')
+      if ( bigKeys.includes(curArray[i]) ) {
+        newKey.classList.add('key-big') 
+      }
+      if (layout[i] == ' ' ) {
+        newKey.classList.add('key-big2') 
+      }
+      if ( anotherColorKeys.includes(curArray[i]) ) {
+       newKey.classList.add('key-another-color') 
+      }
+      newKey.textContent =`${curArray[i]}`;
+      keyboardLines[line].appendChild(newKey)
+  }
+}
   changeLangueage()
-  
-   //инициализация клавиатуры
-// initKeys (0,14 , 0)
-// initKeys (14,29 , 1)
-// initKeys (29,42, 2)
-// initKeys (42,56 , 3)
-// initKeys (56, 63, 4)
 
 let keyboardLine  = document.querySelector('.keyboard-line')
 
 
+
+//обработка кликов по клавишам 
 fieldContainer.addEventListener('click', function getGray(e) {
-  if (e.target.classList.contains('key')) {
+  if ((e.target.classList.contains('key'))|| (e.target.classList.contains('key-another-color'))) {
     let target = e.target
-    target.style.backgroundColor = "gray";
-    if (target.classList.contains('key')) {
-      target.classList.add('key-animation')
-    }
+    target.classList.add('key-animation')
+    target.classList.add('gray')
     setTimeout(function getOriginalColor(e){
-      if (target.classList.contains('.key-another-color')) {
-        target.classList
-      }
-    target.style.backgroundColor = "rgb(19, 19, 19)";
+    target.classList.remove('gray')
     target.classList.remove('key-animation')
     }, 300 )
    if (target.textContent == 'Backspace') {
@@ -125,11 +150,28 @@ fieldContainer.addEventListener('click', function getGray(e) {
    } else if (target.textContent == 'Tab') {
     textArea.textContent += '\n '
     textArea.textContent =  (textArea.textContent).slice(0, -1)
-   } 
+   } else if (target.textContent == 'Alt') {
+      console.log('ничего')
+   } else if (target.textContent == 'Shift') {
+    console.log('ничего')
+   } else if (target.textContent == 'Enter') {
+    textArea.textContent += '\n '
+  } else if (target.textContent == 'ControlLeft') {
+    textArea.textContent += '\n '
+    
+   } else if (target.textContent == 'Ctrl') {
+    console.log('ничего   ')
+   } else if (target.textContent == 'CapsLock') {
+    console.log('ничего ')
+   } else if (target.textContent == 'Win') {
+    console.log('ничего ')
+   } else if (target.textContent == 'DEL') {
+    console.log('ничего ')
+   }
    else{
     textArea.textContent += target.textContent
    }
-  }
+  } 
 }
 )
 
@@ -137,8 +179,9 @@ fieldContainer.addEventListener('click', function getGray(e) {
 function changeLangueage() {
   keyboardLines.forEach(el => keyboardContainer.removeChild(el))
   createLine ()
-  if (layoutLanguage === 'ENG') {
-   layoutLanguage = 'RUS';
+  if ( localStorage.getItem('language') === 'ENG') {
+    localStorage.language = 'RUS';
+
    keyboardLines  = document.querySelectorAll(".keyboard-line")
    //инициализация клавиатуры 
    initKeys (0,14 , 0)
@@ -146,8 +189,10 @@ function changeLangueage() {
    initKeys (30,43, 2)
    initKeys (43,56 , 3)
    initKeys (56, 64, 4)
+
+  //  languageIcon.src = "assets/russian.png"
   } else { 
-   layoutLanguage = 'ENG';
+    localStorage.language = 'ENG';
    keyboardLines  = document.querySelectorAll(".keyboard-line")
       //инициализация клавиатуры 
    initKeys (0,14 , 0)
@@ -155,16 +200,12 @@ function changeLangueage() {
    initKeys (29,42, 2)
    initKeys (42,56 , 3)
    initKeys (56, 64, 4)
- 
    }
-   console.log(layoutLanguage);
+  //  console.log(layoutLanguage);
+
+  //  languageIcon.src = "assets/brisish.png"
+   
 }
-
-
-
-
-
-
 
 fieldContainer.addEventListener('click', function getName(e) {
   if (e.target.classList.contains('key')) {
@@ -172,14 +213,42 @@ fieldContainer.addEventListener('click', function getName(e) {
   }  
 })
 
-
-
-
 //запись в текстареа, того, что набрано клавишами
 document.addEventListener('keydown', function getKek(e) {
   console.log(e.key)
-  textArea.textContent += e.key
-  let keys  = document.querySelectorAll('.key')
+    if (bigKeys.includes(e.key)) {
+      console.log('ehhfff')
+    } else {
+      if (e.key == 'ArrowUp') {
+        textArea.textContent += '↑'
+      }
+      else if (e.key == 'ArrowLeft') {
+        textArea.textContent += '←'
+      }
+      else if (e.key == 'Control') {
+        textArea.textContent += ''
+      }
+      else if (e.key == 'ArrowRight') {
+        textArea.textContent += '→'
+      }
+        else if (e.key == 'ArrowDown') {
+          textArea.textContent += '↓'
+        }
+        else if (e.key == 'Alt') {
+          console.log('Нажали на alt')
+        }
+        else if (e.key == ' ') {
+          textArea.textContent += ' '
+        }
+       
+      else { 
+        if (e.key !== 'Meta') {
+      textArea.textContent += e.key
+        }
+      }
+    }
+
+   keys  = document.querySelectorAll('.key')
   keys.forEach(el => {
    if(el.textContent == e.key) {
     el.style.backgroundColor = 'gray'
@@ -189,47 +258,88 @@ document.addEventListener('keydown', function getKek(e) {
     }, 400)
    }
   })
- 
 })
-
 
 function runOnKeys() {
   let pressed = new Set();
-
   document.addEventListener('keydown', function(event) {
     console.log(event.code)
     pressed.add(event.code);
-    let codes = ['AltLeft', 'ShiftLeft']
+    let codes = ['AltLeft', 'ControlLeft']
     for (let code of codes) { 
       if (!pressed.has(code)) {
         return;
       }
     }
-  
     pressed.clear();
-
     changeLangueage()
   });
-
   document.addEventListener('keyup', function(event) {
     pressed.delete(event.code);
   });
-
 }
+
 runOnKeys()
-
-
-
 
 // change Language 
 
+
+document.addEventListener('keydown', function isCaps(e) {
+  if (e.code == 'ShiftLeft' || e.code == 'ShiftRight') { 
+ 
+    keys.forEach(el => {
+      if(el.textContent == e.key) {
+       el.classList.add('red')
+    console.log('kek hello')
+      }})
+      changeLangueage()
+  } 
+
+} )
+
+
+document.addEventListener('keyup', function isCaps(e) {
+  if (e.code == 'ShiftLeft' || e.code == 'ShiftRight') {
+    localStorage.language = localStorage.language == 'ENG' ? 'RUS' : 'ENG'  
+    keys.forEach(el => {
+      if(el.textContent == e.key) {
+       el.classList.remove('red')
+    console.log('kek hello')
+      }})
+      
+      changeLangueage()
+  } 
+  else if (e.code == 'CapsLock') {
+    if (e.code == 'CapsLock') {  
+      localStorage.language = localStorage.language == 'ENG'? 'RUS': 'ENG'
+      if (capsEnaible == false) {
+        keys.forEach(el => {
+          if(el.textContent == e.code) {
+           el.classList.add('red')
+            
+          } })
+
+          changeLangueage()
+          capsEnaible = true;
+      } else {
+        keys.forEach(el => {
+          if(el.textContent == e.code) {
+           el.classList.remove('red')
+          } })
+          changeLangueage()
+          capsEnaible = false;
+      }
+    }
+   
+  } 
+} )
+
+
+
 languageIcon.addEventListener('click', runOnKeys)
-
-
-
-
 
 // document.addEventListener('click', (e) => console.log(e.code))
 
 
   
+
